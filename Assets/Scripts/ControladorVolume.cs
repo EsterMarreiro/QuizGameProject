331dddb03ecca84c35846e18ef027a2c2091a558
Ester Marreiro <estermarreiro@outlook.com>
2026-04-22 08:36:34 -0300

    fix: :bug: Corrige erros de controlador de som e texto do temporizador

--- Assets/Scripts/ControladorVolume.cs
+++ Assets/Scripts/ControladorVolume.cs
@@ -4,15 +4,29 @@ using UnityEngine.UI;
 public class ControladorVolume : MonoBehaviour
 {
     public Slider slider;
-    public AudioSource musicaFundo;
+
+    private AudioSource musicaFundo;
 
     void Start()
+{
+    GameObject obj = GameObject.Find("BackgroundSound");
+    if (obj != null)
     {
+        Debug.Log("BackgroundSound encontrado!");
+        musicaFundo = obj.GetComponent<AudioSource>();
         slider.value = musicaFundo.volume;
     }
+    else
+    {
+        Debug.Log("BackgroundSound NAO encontrado!");
+    }
+}
 
     public void MudarVolume(float valor)
     {
-        musicaFundo.volume = valor;
+        if (musicaFundo != null)
+        {
+            musicaFundo.volume = valor;
+        }
     }
 }
